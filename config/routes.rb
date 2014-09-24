7010458236ff01324f4565c340075e6fbe989828
Ridhwana <Ridhwana.khan16@gmail.com>
2014-09-24 17:27:59 +0200

    exercise: add a second action called goodbye that renders the texgoodbye world

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   # Note: application is the controller and hello the action
-  root 'application#hello'
-
+  #root 'application#hello'
+  root 'application#goodbye'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
